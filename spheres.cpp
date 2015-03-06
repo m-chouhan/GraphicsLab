fd0b0f27376f2d596ee70b9f66be005f46d67897
montyc008 <montyc008@gmail.com>
2015-03-07 00:09:37 +0530

    resturctuing simulater.hpp

--- spheres.cpp
+++ spheres.cpp
@@ -11,31 +11,11 @@ using namespace std;
 int main(int argc, char **argv) {
             
       SimulatorInit(argc,argv,500,500);
-      Sphere S( Point3D(0.0,0.0,0),4.5,10 ) ,S2( Point3D(-10,1) , 5.2,10);
-      //S.origin = Point3D(0.9,0.9,0);
+      Sphere S( Point3D(0.0,0.0,0),4.5,10 ) ,S2( Point3D(-10,1,20) , 5.2,10);
       World.push_back( &S);
       World.push_back( &S2);
-      cout<<S.getOrigin()<<S.getRad();
-      //Simulator::StartSimulation();
-      
-      /*
-	// init GLUT and create window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(Width,Height);
-	glutCreateWindow("Rotating Spheres");
-
-	// register callbacks
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
-	glutIdleFunc(renderScene);
-
-	// here are the new entries
-	glutKeyboardFunc(processNormalKeys);
-	glutSpecialFunc(processSpecialKeys);
-      */
+      //cout<<S.getOrigin()<<S.getRad();
+      //Simulator::StartSimulation();      
       glutMainLoop();
-	// enter GLUT event processing cycle
 	return 1;
 }
--- src/Simulator.hpp
+++ src/Simulator.hpp
@@ -8,20 +8,21 @@
 #include "Sphere.hpp"
 #include "Cube.hpp"
 
+//class Simulator {
+      
+            extern int Width,Height;
+            extern ShapeList World;
+            extern Point3D CamVector;
 
-extern int Width,Height;
-extern ShapeList World;
-extern Point3D CamVector;
+            void Reshape(int w, int h) ;
+            void RenderScene(void);
+            void NormalKeyEvent(unsigned char key, int x, int y) ;
+            void SpecialKeyEvent(int key, int x, int y) ;
+            void MouseEvent(int button, int state, int x, int y);
+            void drawSphere(Sphere &s);
+            void drawCube(Cube &c);
+            void draw2DFrame(void);
 
-void Reshape(int w, int h) ;
-void RenderScene(void);
-void NormalKeyEvent(unsigned char key, int x, int y) ;
-void SpecialKeyEvent(int key, int x, int y) ;
-void MouseEvent(int button, int state, int x, int y);
-void drawSphere(Sphere &s);
-void drawCube(Cube &c);
-void draw2DFrame(void);
-
-void SimulatorInit(int argc, char *argv[],int W,int H);
-            
+            void SimulatorInit(int argc, char *argv[],int W,int H);
+//};            
 #endif
